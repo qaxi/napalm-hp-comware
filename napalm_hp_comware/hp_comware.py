fb930a4f3c2aaeeb5352ce769ea9335b29cefc95
Zhechev <zhechev.zhecho@gmail.com>
2017-07-11 18:39:31 +0300

    first teset commit steps ;) 	modified:   napalm_hp_comware/hp_comware.py

--- napalm_hp_comware/hp_comware.py
+++ napalm_hp_comware/hp_comware.py
@@ -3,6 +3,9 @@ Napalm driver for HpComware Devices
 
 Read https://napalm.readthedocs.io for more information.
 """
+from netmiko import ConnectHandler, FileTransfer, InLineTransfer
+from netmiko import __version__ as netmiko_version
+
 
 from napalm_base.base import NetworkDriver
 from napalm_base.exceptions import (
@@ -28,10 +31,61 @@ class HpComwareDriver(NetworkDriver):
         if optional_args is None:
             optional_args = {}
 
+        # Netmiko possible arguments
+        netmiko_argument_map = {
+            'port': None,
+            'secret': '',
+            'verbose': False,
+            'keepalive': 30,
+            'global_delay_factor': 1,
+            'use_keys': False,
+            'key_file': None,
+            'ssh_strict': False,
+            'system_host_keys': False,
+            'alt_host_keys': False,
+            'alt_key_file': '',
+            'ssh_config_file': None,
+        }
+
+        fields = netmiko_version.split('.')
+        fields = [int(x) for x in fields]
+        maj_ver, min_ver, bug_fix = fields
+        if maj_ver >= 2:
+            netmiko_argument_map['allow_agent'] = False
+        elif maj_ver == 1 and min_ver >= 1:
+            netmiko_argument_map['allow_agent'] = False
+
+        # Build dict of any optional Netmiko args
+        self.netmiko_optional_args = {}
+        for k, v in netmiko_argument_map.items():
+            try:
+                self.netmiko_optional_args[k] = optional_args[k]
+            except KeyError:
+                pass
+        self.global_delay_factor = optional_args.get('global_delay_factor', 1)
+
+        self.device = None
+        self.interface_map = {}
+
+ 
     def open(self):
-        """Implementation of NAPALM method open."""
-        pass
+        """Open a connection to the device."""
+        self.device = ConnectHandler(device_type='hp_comware',
+                                     host=self.hostname,
+                                     username=self.username,
+                                     password=self.password,
+                                     **self.netmiko_optional_args)
+        # ensure in enable mode
+        self.device.enable()
+        if not self.dest_file_system:
+            try:
+                self.dest_file_system = self.device._autodetect_fs()
+            except AttributeError:
+                raise AttributeError("Netmiko _autodetect_fs not found please upgrade Netmiko or "
+                                     "specify dest_file_system in optional_args.")
 
     def close(self):
-        """Implementation of NAPALM method close."""
-        pass
+        """Close the connection to the device."""
+        self.device.disconnect()
+
+
