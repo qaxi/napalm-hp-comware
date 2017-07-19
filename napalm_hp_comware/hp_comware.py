d74061fb48436604f6f2c3642f3397b24a05977b
Zhechev <zhechev.zhecho@gmail.com>
2017-07-19 18:47:34 +0300

    Adding methods     disable_pageing()     get_mac_address_table()     format_mac_cisco_way()     get_arp_table_()     normalize_port_name()     get_interfaces_ip()
    	modified:   napalm_hp_comware/hp_comware.py

--- napalm_hp_comware/hp_comware.py
+++ napalm_hp_comware/hp_comware.py
@@ -109,7 +109,7 @@ class HpComwareDriver(NetworkDriver):
 
     
     def _generate_ssh_proxy_file(self):
-        filename = 'ssh_proxy_'+ self.hostname
+        filename = '/var/tmp/ssh_proxy_'+ self.hostname
         fh = open(filename, 'w')
         fh.write('Host '+ self.hostname + '\n')
         fh.write('HostName '+ self.hostname + '\n')
@@ -134,6 +134,17 @@ class HpComwareDriver(NetworkDriver):
     def close(self):
         """Close the connection to the device."""
         self.device.disconnect()
+
+
+    def disable_pageing(self):
+        """ Disable pageing on the device """
+        out_disable_pageing = self.device.send_command('screen-length disable')
+        if 'configuration is disabled for current user' in out_disable_pageing:
+            pass
+        else:
+            raise ValueError("Disable Pageing cli command error: {}".format(out_disable_pageing))
+            sys.exit(" --- Exiting: try to workaround this ---")
+
 
     def get_facts(self):
         """
@@ -161,13 +172,7 @@ class HpComwareDriver(NetworkDriver):
             }
 
         """
-        out_disable_pageing = self.device.send_command('screen-length disable')
-        if 'configuration is disabled for current user' in out_disable_pageing:
-            pass
-        else:
-            raise ValueError("Disable Pageing cli command error: {}".format(out_disable_pageing))
-            sys.exit(" --- Exiting: try to workaround this ---")
-
+        self.disable_pageing()
         out_display_version = self.device.send_command("display version").split("\n")
         for line in out_display_version: 
             if "Software, Version " in line:
@@ -194,7 +199,7 @@ class HpComwareDriver(NetworkDriver):
                         uptime += int(timer[0]) * self._MINUTE_SECONDS
 
         out_display_device = self.device.send_command("display device manuinfo")
-        match = re.findall(r"""^Slot\s+(\d+):\nDEVICE_NAME\s+:\s+(.*)\nDEVICE_SERIAL_NUMBER\s+:\s+(.*)\nMAC_ADDRESS\s+:\s+([0-9A-F]{1,4}-[0-9A-F]{1,4}-[0-9A-F]{1,4})\nMANUFACTURING_DATE\s+:\s+(.*)\nVENDOR_NAME\s+:\s+(.*)""",out_display_device,re.M)
+        match = re.findall(r"""^Slot\s+(\d+):\nDEVICE_NAME\s+:\s+(.*)\nDEVICE_SERIAL_NUMBER\s+:\s+(.*)\nMAC_ADDRESS\s+:\s+([0-9a-fA-F]{1,4}-[0-9a-fA-F]{1,4}-[0-9a-fA-F]{1,4})\nMANUFACTURING_DATE\s+:\s+(.*)\nVENDOR_NAME\s+:\s+(.*)""",out_display_device,re.M)
         snumber = set()
         vendor = set()
         hwmodel = set()
@@ -218,6 +223,241 @@ class HpComwareDriver(NetworkDriver):
           "interface_list": interfaces
         }
         return facts
+
+    def get_mac_address_table(self):
+
+        """
+        Returns a lists of dictionaries. Each dictionary represents an entry in the MAC Address
+        Table, having the following keys:
+            * mac (string)
+            * interface (string)
+            * vlan (int)
+            * active (boolean)
+            * static (boolean)
+            * moves (int)
+            * last_move (float)
+
+        However, please note that not all vendors provide all these details.
+        E.g.: field last_move is not available on JUNOS devices etc.
+
+        Example::
+
+            [
+                {
+                    'mac'       : '00:1C:58:29:4A:71',
+                    'interface' : 'Ethernet47',
+                    'vlan'      : 100,
+                    'static'    : False,
+                    'active'    : True,
+                    'moves'     : 1,
+                    'last_move' : 1454417742.58
+                },
+                {
+                    'mac'       : '00:1C:58:29:4A:C1',
+                    'interface' : 'xe-1/0/1',
+                    'vlan'       : 100,
+                    'static'    : False,
+                    'active'    : True,
+                    'moves'     : 2,
+                    'last_move' : 1453191948.11
+                },
+                {
+                    'mac'       : '00:1C:58:29:4A:C2',
+                    'interface' : 'ae7.900',
+                    'vlan'      : 900,
+                    'static'    : False,
+                    'active'    : True,
+                    'moves'     : None,
+                    'last_move' : None
+                }
+            ]
+        """
+        # Disable Pageing of the device
+        self.disable_pageing()
+
+        # <FRGREACHPES002>display mac-address
+        # MAC ADDR       VLAN ID  STATE          PORT INDEX               AGING TIME(s)
+        # 2c41-3888-24a7 1        Learned        Bridge-Aggregation30     AGING
+        # a036-9f00-1dfa 1        Learned        Bridge-Aggregation30     AGING
+        # a036-9f00-29c5 1        Learned        Bridge-Aggregation31     AGING
+        # a036-9f00-29c6 1        Learned        Bridge-Aggregation31     AGING
+        # b8af-675c-0800 1        Learned        Bridge-Aggregation2      AGING
+        out_mac_table = self.device.send_command('display mac-address')
+        mactable = re.findall(r'^([0-9a-fA-F]{1,4}-[0-9a-fA-F]{1,4}-[0-9a-fA-F]{1,4})\s+(\d+)\s+(\w+)\s+([A-Za-z0-9-/]{1,40})\s+(.*)',out_mac_table,re.M)
+        output_mactable = []
+        record = {}
+        for rec in mactable:
+            mac,vlan,state,port,aging = rec
+            record['mac'] = self.format_mac_cisco_way(mac)
+            record['interface'] = self.normalize_port_name(port)
+            record['vlan'] = vlan
+            record['static'] = 'None'
+            record['active'] = 'None'
+            record['moves'] = 'None'
+            record['last_move'] = 'None'
+            output_mactable.append(record)
+        return output_mactable     
+
+
+    def format_mac_cisco_way(self,macAddress):
+        """ 
+        function formating mac address to cisco form 
+        AA:BB:CC:DD:EE:FF
+        """
+        macAddress = macAddress.replace('-','')
+        return macAddress[:2] +\
+                ':'+macAddress[2:4]+\
+                ':'+macAddress[4:6]+\
+                ':'+macAddress[6:8]+\
+                ':'+macAddress[8:10]+\
+                ':'+macAddress[10:12]
+
+    def get_arp_table(self):
+
+        """
+        Returns a list of dictionaries having the following set of keys:
+            * interface (string)
+            * mac (string)
+            * ip (string)
+            * age (float)
+
+        Example::
+
+            [
+                {
+                    'interface' : 'MgmtEth0/RSP0/CPU0/0',
+                    'mac'       : '5C:5E:AB:DA:3C:F0',
+                    'ip'        : '172.17.17.1',
+                    'age'       : 1454496274.84
+                },
+                {
+                    'interface' : 'MgmtEth0/RSP0/CPU0/0',
+                    'mac'       : '5C:5E:AB:DA:3C:FF',
+                    'ip'        : '172.17.17.2',
+                    'age'       : 1435641582.49
+                }
+            ]
+
+        """
+        # Disable Pageing of the device
+        self.disable_pageing()
+        out_arp_table = self.device.send_command('display arp')
+        arptable = re.findall(r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+([0-9a-fA-F]{1,4}-[0-9a-fA-F]{1,4}-[0-9a-fA-F]{1,4})\s+(\d+)\s+([A-Za-z0-9-/]{1,40})\s+(\d+)\s+(\w+)\n',out_arp_table,re.M)
+        output_arptable = []
+        record = {}
+        for rec in arptable:
+            ip,mac,vlan,port,aging,arp_type = rec
+            record['interface'] = self.normalize_port_name(port)
+            record['mac'] = self.format_mac_cisco_way(mac)
+            record['ip'] = ip
+            record['vlan'] = vlan
+            record['aging'] = aging 
+            output_arptable.append(record)
+        return output_arptable     
+
+
+    def normalize_port_name(self,res_port):
+        """ Convert Short HP interface names to long (ex: BAGG519 --> Bridge-Aggregation 519)"""
+        if re.match('^BAGG\d+',res_port):
+            # format port BAGG519 --> Bridge-Aggregation 519
+            agg_port_name = res_port.replace('BAGG','Bridge-Aggregation ')
+            return agg_port_name
+        elif re.match('^Bridge-Aggregation\d*',res_port):
+            agg_port_name = res_port
+            return agg_port_name
+        elif re.match('^XGE\d*',res_port):
+            # format port XGE1/2/0/7 --> Ten-GigabitEthernet 1/2/0/7
+            port_name = res_port.replace('XGE','Ten-GigabitEthernet ')
+            # print(" --- Port Name: "+'\x1b[1;32;40m' +"{}" .format(port_name)+'\x1b[0m')
+            return port_name
+        elif re.match('^GE\d*',res_port):
+            # format port GE1/5/0/19 --> GigabitEthernet 1/5/0/19
+            port_name = res_port.replace('GE','GigabitEthernet ')
+            # print(" --- Port Name: "+'\x1b[1;32;40m' +"{}" .format(port_name)+'\x1b[0m')
+            return port_name
+        elif re.match('^Vlan\d+',res_port):
+            # format port Vlan4003 --> Vlan-interface4003
+            port_name = res_port.replace('Vlan','Vlan-interface')
+            # print(" --- Port Name: "+'\x1b[1;32;40m' +"{}" .format(port_name)+'\x1b[0m')
+            return port_name
+        else:
+            return res_port 
+            # print('\x1b[1;31;40m' + " --- Unknown Port Name: {} --- ".format(res_port)+'\x1b[0m')
+
+
+    def get_interfaces_ip(self):
+
+        """
+        Returns all configured IP addresses on all interfaces as a dictionary of dictionaries.
+        Keys of the main dictionary represent the name of the interface.
+        Values of the main dictionary represent are dictionaries that may consist of two keys
+        'ipv4' and 'ipv6' (one, both or none) which are themselvs dictionaries witht the IP
+        addresses as keys.
+        Each IP Address dictionary has the following keys:
+            * prefix_length (int)
+
+        Example::
+
+            {
+                u'FastEthernet8': {
+                    u'ipv4': {
+                        u'10.66.43.169': {
+                            'prefix_length': 22
+                        }
+                    }
+                },
+                u'Loopback555': {
+                    u'ipv4': {
+                        u'192.168.1.1': {
+                            'prefix_length': 24
+                        }
+                    },
+                    u'ipv6': {
+                        u'1::1': {
+                            'prefix_length': 64
+                        },
+                        u'2001:DB8:1::1': {
+                            'prefix_length': 64
+                        },
+                        u'2::': {
+                            'prefix_length': 64
+                        },
+                        u'FE80::3': {
+                            'prefix_length': u'N/A'
+                        }
+                    }
+                },
+                u'Tunnel0': {
+                    u'ipv4': {
+                        u'10.63.100.9': {
+                            'prefix_length': 24
+                        }
+                    }
+                }
+            }
+        """
+        # Disable Pageing of the device
+        self.disable_pageing()
+       
+        out_curr_config = self.device.send_command('display current-configuration')
+        ipv4table = re.findall(r'^interface\s+([A-Za-z0-9-/]{1,40})\n.*\s+ip\s+address\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\n',out_curr_config,re.M)
+        # TODO: get device with v6 and update above struct
+        # ipv6table = re.findall(r'',out_curr_config,re.M)
+        output_ipv4table = []
+        iface = {}
+        iface['ipv4'] = {}
+        iface['ipv6'] = {}
+        for rec in ipv4table:
+            interface,ip,mask = rec
+            norm_int = self.normalize_port_name(interface)
+            from IPython.core import debugger ;debug = debugger.Pdb().set_trace; debug()
+            # iface['ipv4'] = norm_int[ip['prefix_length'] = mask ]
+            iinterfaces = { norm_int : {'ipv4': {ip: { 'prefix_len': mask}}}}
+            output_ipv4table.append(iinterfaces)
+
+        return output_ipv4table     
+
+
 
 
 
